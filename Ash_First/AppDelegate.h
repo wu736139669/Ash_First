b9421d103be17538be6117ceb00acdfed6a70b2d
wu736139669 <736139669@qq.com>
2014-07-01 15:38:15 +0800

    first

---
+++ Ash_First/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ash_First
+//
+//  Created by xmfish on 14-7-1.
+//  Copyright (c) 2014年 xmfish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ash_FirstTests/Ash_FirstTests.m
@@ -0,0 +1,34 @@
+//
+//  Ash_FirstTests.m
+//  Ash_FirstTests
+//
+//  Created by xmfish on 14-7-1.
+//  Copyright (c) 2014年 xmfish. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ash_FirstTests : XCTestCase
+
+@end
+
+@implementation Ash_FirstTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
